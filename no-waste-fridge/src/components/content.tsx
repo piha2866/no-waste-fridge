5b90cead3dc05041788d3b32df6742680ecd47d2
pia halberstadt <pia.halberstadt@beamery.com>
2025-03-17 21:31:58 +0100

    chore: add default food image

--- no-waste-fridge/src/components/content.tsx
+++ no-waste-fridge/src/components/content.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
+import colors from "../styles/colors";
 
 export function Content(props: any): React.JSX.Element {
   return (
     <View style={styles.contentContainer}>
         <View>
-          <Image source={require("../assets/images/default-food.png")} />
+          <Image source={require("../assets/images/default-food.png")}  style={styles.image} resizeMode="contain" />
         </View>
         <View>
-            <Text>{props.name}</Text>
+            <Text style={styles.text}>{props.name}</Text>
         </View>
     </View>
   );
@@ -20,8 +21,12 @@ const styles = StyleSheet.create({
         margin: 10,
         borderRadius: 20,
         width: 100,
-        height: 100,
+        height: 120,
         justifyContent: "center",
         alignItems: "center",
+    },
+    image: { width: 80, height: 80 },
+    text: {
+      color: colors.text
     }
 });
