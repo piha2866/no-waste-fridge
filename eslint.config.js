bd6d756825bf331d625734abcfa426995b98cce8
Pia Halberstadt <pia.halberstadt@gmail.com>
2025-06-04 14:02:04 +0000

    chore: add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@ import eslintPluginReact from 'eslint-plugin-react';
 import eslintPluginReactNative from 'eslint-plugin-react-native';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+import jest from 'eslint-plugin-jest';
 
 export default [
   // ESLint core
@@ -11,8 +14,8 @@ export default [
 
   // TypeScript plugin with recommended settings
   ...tseslint.configs.recommended,
-//   ...tseslint.configs.recommendedTypeChecked,
-//   ...tseslint.configs.stylisticTypeChecked,
+  //   ...tseslint.configs.recommendedTypeChecked,
+  //   ...tseslint.configs.stylisticTypeChecked,
 
   {
     files: ['**/*.ts', '**/*.tsx'],
@@ -25,6 +28,8 @@ export default [
       react: eslintPluginReact,
       'react-native': eslintPluginReactNative,
       prettier: eslintPluginPrettier,
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
     },
     rules: {
       // React
@@ -33,17 +38,60 @@ export default [
 
       // React Native
       'react-native/no-inline-styles': 'warn',
+      'react-native/no-raw-text': 'error',
+      'react-native/no-unused-styles': 'warn',
 
       // TypeScript
+      'no-console': 'warn',
+      'no-empty': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'error',
 
       // Prettier
       'prettier/prettier': 'error',
+
+      //Import export sorting
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
     settings: {
       react: {
         version: 'detect',
       },
+    },
+  },
+  {
+    files: ['**/__tests__/**/*.ts', '**/*.test.ts', '**/*.test.tsx'],
+    plugins: {
+      jest,
+    },
+    languageOptions: {
+      globals: {
+        // Add Jest globals like describe, test, expect, etc.
+        jest: true,
+        describe: true,
+        it: true,
+        expect: true,
+        beforeEach: true,
+        afterEach: true,
+        beforeAll: true,
+        afterAll: true,
+      },
+    },
+    rules: {
+      // 🧪 Recommended Jest rules
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+      'jest/expect-expect': 'warn',
+      'jest/no-deprecated-functions': 'warn',
+      'jest/no-conditional-expect': 'warn',
+      'jest/prefer-hooks-in-order': 'warn',
+      'jest/prefer-strict-equal': 'warn',
     },
   },
 ];
