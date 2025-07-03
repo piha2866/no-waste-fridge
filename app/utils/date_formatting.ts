73f7f134884b01cb189046e5948090c4170c4c14
Pia Halberstadt <pia.halberstadt@gmail.com>
2025-07-03 14:17:24 +0000

    chore: add function to automatically determine new expiration date based on old opening and expiration dates

--- app/utils/date_formatting.ts
+++ app/utils/date_formatting.ts
@@ -6,8 +6,31 @@ export function formatDateToDDMMYYYY(date: Date): string {
     if (day === 'NaN') throw Error('Invalid date');
     return `${day}-${month}-${year}`;
   } catch (error) {
-    console.log('DATEE', date);
     console.log(error);
     return '';
   }
+}
+
+export function calcNewExpirationDate(
+  openingDate: Date,
+  expirationDate: Date,
+  newOpeningDate = new Date(),
+): Date {
+  const croppedOpeningDate = new Date(
+    openingDate.getFullYear(),
+    openingDate.getMonth(),
+    openingDate.getDate(),
+  );
+  const croppedExpirationDate = new Date(
+    expirationDate.getFullYear(),
+    expirationDate.getMonth(),
+    expirationDate.getDate(),
+  );
+  const croppedNewOpeningDate = new Date(
+    newOpeningDate.getFullYear(),
+    newOpeningDate.getMonth(),
+    newOpeningDate.getDate(),
+  );
+  const diffMs = croppedExpirationDate.getTime() - croppedOpeningDate.getTime();
+  return new Date(croppedNewOpeningDate.getTime() + diffMs);
 }
