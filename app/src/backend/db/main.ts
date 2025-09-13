747b258dbac437f6beff5958fcc3ea1ce8354d97
Pia Halberstadt <pia.halberstadt@gmail.com>
2025-09-13 19:47:50 +0000

    prevent data deletion

--- app/src/backend/db/main.ts
+++ app/src/backend/db/main.ts
@@ -17,16 +17,16 @@ export const createTables = async (db: SQLiteDatabase): Promise<void> => {
     expiration_date TEXT NOT NULL,
     image_location TEXT
     );`;
-  await db.executeSql(`DROP TABLE IF EXISTS ${notesTable};`);
+  // await db.executeSql(`DROP TABLE IF EXISTS ${notesTable};`);
   await db.executeSql(createNotesTableQuery);
   const openingDate = new Date('1-20-2020');
   const expirationDate = new Date('1-23-2020');
-  await db.executeSql(`INSERT INTO ${notesTable} (title, description, opening_date, expiration_date) VALUES
-    ('Groceries', 'Buy milk, eggs, and bread', '${openingDate}', '${expirationDate}'),
-    ('Workout Plan', 'Monday: chest, Tuesday: back', '${openingDate}', '${expirationDate}'),
-    ('Meeting Notes', 'Discuss project roadmap and milestones', '${openingDate}', '${expirationDate}'),
-    ('Books to Read', 'The Great Gatsby, 1984, Brave New World', '${openingDate}', '${expirationDate}'),
-    ('Ideas', 'App for tracking daily habits', '${openingDate}', '${expirationDate}');`);
+  // await db.executeSql(`INSERT INTO ${notesTable} (title, description, opening_date, expiration_date) VALUES
+  //   ('Groceries', 'Buy milk, eggs, and bread', '${openingDate}', '${expirationDate}'),
+  //   ('Workout Plan', 'Monday: chest, Tuesday: back', '${openingDate}', '${expirationDate}'),
+  //   ('Meeting Notes', 'Discuss project roadmap and milestones', '${openingDate}', '${expirationDate}'),
+  //   ('Books to Read', 'The Great Gatsby, 1984, Brave New World', '${openingDate}', '${expirationDate}'),
+  //   ('Ideas', 'App for tracking daily habits', '${openingDate}', '${expirationDate}');`);
 };
 
 export const initializeDB = async (): Promise<SQLiteDatabase> => {
